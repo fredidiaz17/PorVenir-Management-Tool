c87cdab3d84140a595a9bb9b78b0b8d997f6eec5
Fredi <fredidiazcardona0611@gmail.com>
2026-01-30 19:51:02 -0500

    Productos: Controlador y modelo creados y terminados

---
+++ src/controllers/ProductosController.py
@@ -0,0 +1,55 @@
+from src.logger_config import logger
+from src.models.producto import Producto
+
+class ProductoController:
+
+    def __init__(self, marcas_controller):
+        self.marcas_controller = marcas_controller
+
+    # CRUD producto
+    @staticmethod
+    def crear_producto(
+            nombre, cantidad_stock, unidad_medida, precio_compra, precio_venta, porcentaje_iva, id_marca
+    ):
+        try:
+            logger.info(f'Creando producto: {nombre}')
+            return Producto.crear_producto(nombre, cantidad_stock, unidad_medida, precio_venta, porcentaje_iva, id_marca)
+        except Exception:
+            logger.error(f'Error al crear producto: {nombre}')
+        return False
+
+    @staticmethod
+    def listar_productos():
+        try:
+            logger.info(f'Listando productos')
+            return Producto.listar_productos()
+        except Exception:
+            logger.error(f'Error al listar productos')
+        return False
+
+    @staticmethod
+    def actualizar_producto(
+            id_producto, nombre, cantidad_stock, unidad_medida, precio_compra, precio_venta, porcentaje_iva, id_marca
+    ):
+        try:
+            logger.info(f'Actualizando producto: {nombre}')
+            return Producto.actualizar_producto(
+                id_producto,nombre, cantidad_stock, unidad_medida, precio_compra, precio_venta, porcentaje_iva, id_marca
+            )
+        except Exception:
+            logger.error(f'Error al actualizar producto: {nombre}')
+        return False
+
+    @staticmethod
+    def eliminar_producto(id_producto):
+        try:
+            logger.info(f'Eliminando producto: #{id_producto}')
+            return Producto.eliminar_producto(id_producto)
+        except Exception:
+            logger.error(f'Error al eliminar producto: #{id_producto}')
+        return False
+
+    # Marcas
+    @staticmethod
+    def listar_marcas(self):
+        return self.marcas_controller.listar_marcas()
