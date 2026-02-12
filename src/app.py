fef0ce3317bb7672af5e977ec1be17dac405ae57
Fredi <fredidiazcardona0611@gmail.com>
2026-02-12 18:56:25 -0500

    vistas agregadas y listas para ser desplegadas, nueva forma de navegación agregada (un navbar con botones por vista), funciones por y para botones, ademas de otros cambios

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
 import customtkinter as ctk
 
+
 # Importar vistas
-from views.CompaniasView import CompaniaView
+from views.CompaniasView import CompaniasView
 from views.MarcasView import MarcasView
+from src.views.ProductosView import ProductosView
 
 # Importar Controladores
 from controllers.CompaniasController import CompaniasController
 from controllers.MarcasController import MarcasController
+from controllers.ProductosController import ProductoController
 
 class App(ctk.CTk):
     def __init__(self):
@@ -17,17 +20,24 @@ class App(ctk.CTk):
         self.geometry("900x600")
         self.minsize(800, 500)
 
-        # Contenedor principal (donde viven las vistas)
+        # Configuración del grid principal
+        self.grid_rowconfigure(0, weight=0) # El de la barra (no crece)
+        self.grid_rowconfigure(1, weight=1)  # Vistas (si crece)
+        self.grid_columnconfigure(0, weight=1)
+
+
+        # -------- Contenedor principal (donde viven las vistas) --------
         self.container = ctk.CTkFrame(self)
-        self.container.pack(fill="both", expand=True)
+        self.container.grid(row=1, column=0, sticky="nsew")
 
         # Permitir que las vistas se expandan
-        self.container.rowconfigure(0, weight=1)
-        self.container.columnconfigure(0, weight=1)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
 
         # Controladores por vista
         self.compania_controller = CompaniasController()
         self.marca_controller = MarcasController(self.compania_controller)
+        self.producto_controller = ProductoController(self.marca_controller)
 
         # Diccionario de vistas
         self.vistas = {}
@@ -35,32 +45,69 @@ class App(ctk.CTk):
         # Crear vistas
         self._crear_vistas()
 
+        # -------- Barra de navegación --------
+        self.navbar = ctk.CTkFrame(self, height=50)
+        self.navbar.grid(row=0, column=0, sticky="ew")
+        self.navbar.grid_propagate(False)
+
+        # Botones para cambiar de vista
+        self.botones_nav = {}
+        self._crear_btns()
+
         # Mostrar vista inicial
-        self.mostrar_vista("compañia")
-        self.mostrar_vista("marca")
+        self.vista_actual  = None
+        self.mostrar_vista("producto")
 
 
     def _crear_vistas(self):
 
-        # Instanciar todas las vistas y dejarlas listas...aunque solo haya una por ahora :v
-        self.vistas["compañia"] = CompaniaView(self.container) #agregar app al inicializador de la clase
+        # Instanciar todas las vistas y dejarlas listas
+        self.vistas["compañia"] = CompaniasView(parent=self.container, app=self, controller=self.compania_controller)
         self.vistas["marca"] = MarcasView(parent=self.container, app= self, controller= self.marca_controller)
+        self.vistas["producto"] = ProductosView(parent=self.container, app= self, controller= self.producto_controller)
 
 
-        # acomodandolas (grid), iterando por el diccionario (sus valores)
-        for vista in self.vistas.values():
-            vista.pack(side="bottom")
+    def _crear_btns(self):
+
+        for nombre_vista in self.vistas.keys():
+            btn = ctk.CTkButton(
+                self.navbar,
+                text = nombre_vista,
+                command = lambda n=nombre_vista: self.mostrar_vista(n)
+            )
+
+            btn.pack(side="left", padx=10, pady=10)
+
+            self.botones_nav[nombre_vista] = btn
+
 
     def mostrar_vista(self, nombre_vista):
+        # Destruyendo la vista actual
+        if self.vista_actual is not None:
+            self.vista_actual.grid_forget()
+
         # levantando (raise) la vista
         vista = self.vistas.get(nombre_vista)
+
         if vista:
-            vista.tkraise()
+            vista.grid(row=0, column=0, sticky= "nsew")
+            self.vista_actual = vista
+
+            # resetear colores de botones
+            for btn in self.botones_nav.values():
+                btn.configure(fg_color=("gray75", "gray25"))
+
+            # Activar el seleccionado
+            self.botones_nav[nombre_vista].configure(fg_color=("blue", "blue"))
+
+
         else:
             raise ValueError(f"La vista '{nombre_vista}' no existe")
+
 
 
 if __name__ == "__main__":
     app = App()
     app.mainloop()
 
+# todo: Agregar función _on_Show a cada una de las vistas.
