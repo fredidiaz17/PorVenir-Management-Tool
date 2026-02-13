31e2ab40013c47c7bd8ab2a214b606334c4378a8
Fredi <fredidiazcardona0611@gmail.com>
2026-02-12 21:02:00 -0500

    Cambio leve en ProductosController

--- src/controllers/ProductosController.py
+++ src/controllers/ProductosController.py
@@ -4,10 +4,10 @@ from src.models.enums import productos_enum
 
 class ProductoController:
     # Todo: Anexar validación de datos a nivel controlador.
-    def __init__(self, marcas_controller):
+    def __init__(self, marcas_controller = None):
         self.marcas_controller = marcas_controller
 
-
+g
     # CRUD producto
     @staticmethod
     def crear_producto(
