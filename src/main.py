5e7363655982103c13fe6c365edb2de8443a06f3
Fredi <fredidiazcardona0611@gmail.com>
2026-07-07 19:14:20 -0500

    feat(main): Create API entrypoint

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from api.v1.compania import router as compania_router
 
-app = FastAPI()
+app = FastAPI() # Punto de entrada 
+
+app.include_router( # Incluir router al app
+    compania_router, # Router en cuestión
+    prefix="api/v1/compania", # Prefijo de las rutas del router
+    tags=["Compañias"] # Agrupación visual en documentación (/docs)
+)
 
