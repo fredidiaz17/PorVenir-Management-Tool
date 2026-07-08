5e15937c8803262de297e35a962142b9bd8e3952
Fredi <fredidiazcardona0611@gmail.com>
2026-07-08 17:40:53 -0500

    feat(companias): Add Endpoints for Companias

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
-from api.v1.compania import router as compania_router
+from src.api.v1.compania import router as compania_router
 
-app = FastAPI() # Punto de entrada 
+app = FastAPI(
+    title = "PorvenirMGT API",
+    description = "Aplicación web que permite a tiendas gestionar su inventario de productos.",
+    version = "1.0.0"
+) # Punto de entrada 
 
 app.include_router( # Incluir router al app
     compania_router, # Router en cuestión
-    prefix="api/v1/compania", # Prefijo de las rutas del router
+    prefix="/api/v1/compania", # Prefijo de las rutas del router
     tags=["Compañias"] # Agrupación visual en documentación (/docs)
 )
 
