7efa0d2982a5d7b76cc76be55c6dfea153a08c2f
Fredi <fredidiazcardona0611@gmail.com>
2026-07-17 17:35:33 -0500

    feat: add router to the router_v1 file, add the models of preventista, pedido and detalle_pedido to the models init file. Minor change in the gitignore file

--- src/api_v1.py
+++ src/api_v1.py
@@ -8,6 +8,9 @@ from .routers.v1.producto import router as producto_router
 from .routers.v1.etiqueta import router as etiqueta_router
 from .routers.v1.oferta import router as oferta_router
 from .routers.v1.producto_etiqueta import router as producto_etiqueta_router
+from .routers.v1.preventista import router as preventista_router
+from .routers.v1.pedido import router as pedido_router
+from .routers.v1.detalle_pedido import router as detalle_pedido_router
 
 router = APIRouter()
 
@@ -37,4 +40,19 @@ router.include_router(
     producto_etiqueta_router, 
     prefix="/producto_etiqueta", 
     tags=["Producto Etiquetas"]
+)
+router.include_router(
+    preventista_router, 
+    prefix="/preventista", 
+    tags=["Preventistas"]
+)
+router.include_router(
+    pedido_router, 
+    prefix="/pedido", 
+    tags=["Pedidos"]
+)
+router.include_router(
+    detalle_pedido_router, 
+    prefix="/detalle_pedido", 
+    tags=["Detalle Pedidos"]
 )
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -5,6 +5,9 @@ from .producto import ProductoModel
 from .etiqueta import EtiquetaModel
 from .producto_etiqueta import ProductoEtiquetaModel
 from .oferta import OfertaModel
+from .preventista import PreventistaModel
+from .pedido import PedidoModel
+from .detalle_pedido import DetallePedidoModel
 
 # Opcional, pero permite controlar que expongo al exterior
 __all__ = [
@@ -15,4 +18,7 @@ __all__ = [
     "EtiquetaModel",
     "ProductoEtiquetaModel",
     "OfertaModel",
+    "PreventistaModel",
+    "PedidoModel",
+    "DetallePedidoModel",
 ]
